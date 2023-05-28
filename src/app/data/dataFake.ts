7238a1f162e8496c1c50651b266286389913fa6b
VictorSR117 <victorvidas@hotmail.com>
2023-05-28 18:13:23 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+    {
+        "id": "1",
+        "title": "Nova Armadura do homem de ferro é lancada",
+        "description": "essa armadura tem pelo menos o dobro de resistência da antiga",
+        "photoCover": "https://portalpopline.com.br/wp-content/uploads/2023/03/homem-de-ferro-robert-downey-jr.jpg"
+    },
+    {
+        "id": "2",
+        "title": "Marvel lança suas novas séries do anos",
+        "description": "após o sucesso, as séries da marvel agora ganham novas temporadas com histórias marcantes",
+        "photoCover": "https://streamingsbrasil.com/wp-content/uploads/2020/11/series-marvel-disney-plus-.png"
+    },
+    {
+        "id": "3",
+        "title": "Homem formiga ganha parceiros de campo",
+        "description": "depois de tanto tempo, o Homem formiga finalmente tem uma equipe completa para ajuda-lo em campo.",
+        "photoCover": "https://media.seudinheiro.com/uploads/2023/02/homem-formiga-3-1-e1676650714696.jpg"
+    },
+    {
+        "id": "4",
+        "title": "capitão América demonstra ser digno",
+        "description": "em meio ao caos do campo de batalha, em meio a guerra contra thanos, Steve Rogers preocupado em salvar seus amigos levanta o mjonir para continuar a batalha    ",
+        "photoCover": "https://observatoriodocinema.uol.com.br/wp-content/uploads/2020/06/Captain-America.jpg"
+    },
+]
